3811d7f2f2b91045a8e0992bad0489cd36f3b529
Haider <haideradrees7@gmail.com>
2026-02-22 12:32:17 +0500

    ALL ITEMS

--- assets/css/script.js
+++ assets/css/script.js
@@ -1,67 +1,59 @@
-// owl-carousel
-$('#owl-slider-one').owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: false,
-    dots: true,
-    autoplay: true,
-    autoplayTimeout: 10000,
-    slideTransition: 'linear',
-    autoplaySpeed: 1000,
-    responsive: {
-        0: {
-            items: 1
-        },
-    }
-});
-// owl-carousel
-$('#owl-slider-two').owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: true,
-    dots: true,
-    autoplay: true,
-    autoplayTimeout: 10000,
-    slideTransition: 'linear',
-    autoplaySpeed: 1000,
-    responsive: {
-        0: {
-            items: 1
-        },
-        768: {
-            items: 2
-        },
-        991: {
-            items: 4
-        },
-        1200: {
-            items: 5
-        }
-    }
-});
-// owl-carousel
-
-// Header Active
-$(window).scroll(function () {
-    var scroll = $(window).scrollTop();
-    if (scroll > 0) {
-        $("#header").addClass("active");
-    } else {
-        $("#header").removeClass("active");
-    }
-});
-// Header Active
-
 // AOS
 AOS.init();
 // AOS
 
-// Side Nav
-function openNav() {
-    document.getElementById("mySidenav").style.width = "100%";
-}
+// owl-carousel
+$("#owl-slider-one").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: false,
+  dots: true,
+  autoplay: true,
+  autoplayTimeout: 10000,
+  slideTransition: "linear",
+  autoplaySpeed: 1000,
+  responsive: {
+    0: {
+      items: 1,
+    },
+  },
+});
+// owl-carousel-two
+$("#owl-slider-two").each(function () {
+  var $slider = $(this);
 
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
-// Side Nav
+  // Init carousel
+  $slider.owlCarousel({
+    loop: true,
+    margin: 10,
+    stagePadding: 10,
+    nav: false, // default arrows off
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 10000,
+    slideTransition: "linear",
+    autoplaySpeed: 1000,
+    responsive: {
+      0: {
+        items: 2.1,
+      },
+    },
+  });
+
+  // Next arrow (sirf isi slider ke liye)
+  $slider
+    .closest("section, .slider-wrapper, body") // nearest parent
+    .find(".owl-next-btn")
+    .on("click", function () {
+      $slider.trigger("next.owl.carousel");
+    });
+
+  // Prev arrow (sirf isi slider ke liye)
+  $slider
+    .closest("section, .slider-wrapper, body")
+    .find(".owl-prev-btn")
+    .on("click", function () {
+      $slider.trigger("prev.owl.carousel");
+    });
+});
+// owl-carousel-two
